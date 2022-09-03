34821d712397b4e9a64cb6ada2a7b1b30826f954
adammathewvaughn <adammathewvaughn@gmail.com>
2022-09-02 22:02:46 -0500

    regisering a tie instead of a win if it occurs on the last move

--- app.js
+++ app.js
@@ -1,72 +1,75 @@
 let cells = document.querySelectorAll(".row > div");
-let playerOne = "X"
-let playerTwo = "O"
+let currentplayer = "X"
 let turncount = 0;
 let gameover = false;
 let wincheck = true;
 
 let possibleWins = [
-  [cells[0], cells[1], cells[2]],
-  [cells[3], cells[4], cells[5]],
-  [cells[6], cells[7], cells[8]],
-  [cells[0], cells[3], cells[6]],
-  [cells[1], cells[4], cells[7]],
-  [cells[2], cells[5], cells[8]],
-  [cells[0], cells[4], cells[8]],
-  [cells[2], cells[4], cells[6]]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
+function switchPlayer() {
+  if (currentplayer === "X") {
+    currentplayer = "O";
+  } else {
+    currentplayer = "X";
+  };
+}
 
 cells.forEach(function (cell) {
   cell.addEventListener("click", cellClicked);
-  console.log("click");
 });
 
 function cellClicked(e) {
   if (gameover !== true) {
     let cell = e.target;
+    console.log("clicked")
+    if (cell.textContent !== "") return;
+    cell.textContent = currentplayer
 
-  if (cell.textContent !== "") return;
-  if (turncount % 2 === 0) {
-      e.target.textContent = playerOne
-    } else {
-      e.target.textContent = playerTwo;
-    }
-    if (turncount < 8) {
-      turncount++
-      console.log(turncount)
-      checkForWinner()
-    }
+    turncount++
+    console.log(turncount)
+    checkForWinner()
+    switchPlayer()
   }
 }
 
 function checkForWinner() {
   for (let i = 0; i < possibleWins.length; i++) {
     let combo = possibleWins[i];
-    let p1combocounter = 0;
-    let p2combocounter = 0;
+    let combocounter = 0;
     for (let j = 0; j < combo.length; j++) {
-      if (cells[combo[j]].textContent === playerOne) {
-        p1combocounter++;
-      }
-        if (cells[combo[j]].textContent === playerTwo) {
-          p2combocounter++;
-        }
-      
-      if (p1combocounter === 3) {
-        alert("Player One Wins!")
-      }
-      if (p2combocounter === 3) {
-        alert("Player Two Wins!")
+      if (cells[combo[j]].textContent === currentplayer) {
+        combocounter++;
       }
 
-      if (p1combocounter !== 3) {
-        if (p2combocounter !== 3) {
-          if (turncount < 8) {
+      if (combocounter === 3) {
+        alert(`${currentplayer} Wins!`)
+        gameover = true
+        wincheck = false
+        restartGame();
+      }
+
+      if (combocounter !== 3) {
+        if (wincheck !== false) {
+          if (turncount > 8) {
             alert("It's A Tie!")
+            gameover = true
+            restartGame();
           }
         }
       }
     }
   }
+}
+
+function restartGame() {
+  window.location.reload();
 }
